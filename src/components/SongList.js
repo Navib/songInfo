6cf83ab9dfe4bead56a5ce0cfbeb40a3cf713cc6
navib <ivan.baena.dev@gmail.com>
2019-10-30 22:08:50 -0400

    song detail added

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,19 +1,28 @@
 import React from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
 class SongList extends React.Component {
-  renderSongs() {
-    return (
-      <ul>
-        {this.props.songs.map(song => {
-          return <li key={song.title}>{song.title}</li>;
-        })}
-      </ul>
-    );
+  renderList() {
+    return this.props.songs.map(song => {
+      return (
+        <div className="item" key={song.title}>
+          <div className="right floated content">
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectSong(song)}
+            >
+              Select
+            </button>
+          </div>
+          <div className="content">{song.title}</div>
+        </div>
+      );
+    });
   }
   render() {
     console.log("props", this.props);
-    return <div>{this.renderSongs()}</div>;
+    return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
@@ -25,5 +34,5 @@ const mapStatetoProps = state => {
 
 export default connect(
   mapStatetoProps,
-  null
+  { selectSong }
 )(SongList);
